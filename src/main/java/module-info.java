1af0f59c1f9d8b8024a13b94ed001d566b3d05ed
lowqualityrocketman <lqrm.industries@gmail.com>
2024-04-18 17:13:15 +1000

    added module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.cab302theleftovers {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.cab302theleftovers to javafx.fxml;
