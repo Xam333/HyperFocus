7ab62a708a4b9a94866b5f5730197ece6dd48993
Nuttion <89137024+Nuttion@users.noreply.github.com>
2024-04-23 15:52:03 +1000

    Blocked Page Update

--- src/main/java/com/example/cab302theleftovers/BlockedController.java
+++ src/main/java/com/example/cab302theleftovers/BlockedController.java
@@ -1,57 +1,79 @@
 package com.example.cab302theleftovers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
 
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class BlockedController {
 
-    public Button loginButton;
-    public Button registerButton;
+public class BlockedController implements Initializable {
+
     @FXML
     private ImageView userIcon;
+    @FXML
+    private ImageView youtubeIcon;
+    @FXML
+    private ImageView redditIcon;
 
     @FXML
-    private void initialize() {
+    private TableColumn<BlockedApplication, ImageView> iconColumn;
+
+    @FXML
+    private TableColumn<BlockedApplication, String> nameColumn;
+
+    @FXML
+    private TableColumn<BlockedApplication, String> locationColumn;
+
+    @FXML
+    private TableView<BlockedApplication> tableView;
+
+    ObservableList<BlockedApplication> testData()
+    {
+        BlockedApplication a1 = new BlockedApplication(youtubeIcon, "Youtube", "www.youtube.com");
+        BlockedApplication a2 = new BlockedApplication(redditIcon, "Reddit", "www.reddit.com");
+        return FXCollections.observableArrayList(a1,a2);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
         // Assuming image.png is directly in src/main/resources/
-        Image image = new Image(getClass().getResourceAsStream("/UserIcon.png"));
-        userIcon.setImage(image);
+        Image userImage = new Image(getClass().getResourceAsStream("/UserIcon.png"));
+        userIcon.setImage(userImage);
         userIcon.setFitWidth(20);  // Set the width of the ImageView
-        //imageView.setFitHeight(150); // Set the height of the ImageView
+        //userIcon.setFitHeight(150); // Set the height of the ImageView
         userIcon.setPreserveRatio(true);
+
+
+
+        Image youtubeImage = new Image(getClass().getResourceAsStream("/youtubeIcon.png"));
+        youtubeIcon.setImage(youtubeImage);
+        youtubeIcon.setFitWidth(20);  // Set the width of the ImageView
+        //youtubeIcon.setFitHeight(150); // Set the height of the ImageView
+        youtubeIcon.setPreserveRatio(true);
+
+        Image redditImage = new Image(getClass().getResourceAsStream("/redditIcon.png"));
+        redditIcon.setImage(redditImage);
+        redditIcon.setFitWidth(20);  // Set the width of the ImageView
+        //redditIcon.setFitHeight(150); // Set the height of the ImageView
+        redditIcon.setPreserveRatio(true);
+
+
+
+
+        iconColumn.setCellValueFactory(new PropertyValueFactory<BlockedApplication, ImageView>("iconColumn"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<BlockedApplication, String>("nameColumn"));
+        locationColumn.setCellValueFactory(new PropertyValueFactory<BlockedApplication, String>("locationColumn"));
+
+        tableView.setItems(testData());
+
     }
-
-    @FXML
-    protected void onRegisterButtonClick() throws IOException {
-        Stage stage = (Stage) registerButton.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(""));
-        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
-        stage.setScene(scene);
-    }
-
-    @FXML
-    protected void onLoginButtonClick() throws IOException {
-        Stage stage = (Stage) loginButton.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(""));
-        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
-        stage.setScene(scene);
-    }
-
-
-    public BlockedController(){
-    }
-
 }
