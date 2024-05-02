dbf73de8d33ee8bc1711e94ec5426718fa590d3d
LunaMcConn <niamhmcconn@gmail.com>
2024-05-02 12:07:59 +1000

    Main page functionality

--- src/main/java/focusApp/controllers/MainController.java
+++ src/main/java/focusApp/controllers/MainController.java
@@ -1,20 +1,43 @@
 package focusApp.controllers;
 
+import focusApp.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MainController {
     public Button menuButton;
     public Label startTimeLabel;
     public Label endTimeLabel;
     public Button startButton;
+    public Pane blockedApplicationPane;
 
-    public void onBlockedApplicationsPaneClick(MouseEvent mouseEvent) {
+    public void onBlockedApplicationsPaneClick(MouseEvent mouseEvent) throws IOException {
+        Stage stage = (Stage) blockedApplicationPane.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/blocked-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+
+        // Set scene stylesheet
+        scene.getStylesheets().add(Objects.requireNonNull(HelloApplication.class.getResource("stylesheet.css")).toExternalForm());
+        stage.setScene(scene);
     }
 
-    public void onStartButtonClick(ActionEvent actionEvent) {
+    public void onStartButtonClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) startButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/timer-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+
+        // Set scene stylesheet
+        scene.getStylesheets().add(Objects.requireNonNull(HelloApplication.class.getResource("stylesheet.css")).toExternalForm());
+        stage.setScene(scene);
     }
 
     public void onMenuButtonClick(ActionEvent actionEvent) {
